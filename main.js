4289d5af6e7e825b9a5587c95980791876736e4b
Skorzynski <Kamil.Skorzynski@jacobs.com>
2020-04-20 11:52:05 +0200

    delete whitespace

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
-const btn = document.querySelector('button');
-const names = ['Jagienka', 'Izabela', 'Dobrawa', 'Marysia', 'Anastazja', 'Krystyna', 'Bogumila', 'Genowefa'];
-const div = document.querySelector('div');
-const prefixs = ['Wydaje mi się', 'Mam wrażenie', 'Szczerze powiedziawszy, myślę', 'Szczerze uważam', 'Na 100% twierdzę', 'Jestem pewien']
-
-
-
-
-
-
+const btn = document.querySelector("button");
+const names = [
+  "Jagienka",
+  "Izabela",
+  "Dobrawa",
+  "Marysia",
+  "Anastazja",
+  "Krystyna",
+  "Bogumila",
+  "Genowefa"
+];
+const div = document.querySelector("div");
+const prefixs = [
+  "Wydaje mi się",
+  "Mam wrażenie",
+  "Szczerze powiedziawszy, myślę",
+  "Szczerze uważam",
+  "Na 100% twierdzę",
+  "Jestem pewien"
+];
 
 const nameGenerator = () => {
-    const indexName = Math.floor(Math.random() * names.length);
-    const indexPrefix = Math.floor(Math.random() * prefixs.length);
-    div.textContent = `${prefixs[indexPrefix]}, że najfajniejsze imię to ${names[indexName]}`
-}
+  const indexName = Math.floor(Math.random() * names.length);
+  const indexPrefix = Math.floor(Math.random() * prefixs.length);
+  div.textContent = `${prefixs[indexPrefix]}, że najfajniejsze imię to ${names[indexName]}`;
+};
 
-
-
-btn.addEventListener('click', nameGenerator);
+btn.addEventListener("click", nameGenerator);
